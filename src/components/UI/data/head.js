8e589ad1ea87ca62901b2724d2c36c0253d24e40
rizki <rr8027896@gmail.com>
2024-09-23 00:07:57 +0700

    google tag manager

--- src/components/UI/data/head.js
+++ src/components/UI/data/head.js
@@ -121,7 +121,7 @@ const Head = () => {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TPGTC838');
+})(window,document,'script','dataLayer','GTM-TSXDLB9M');
             `,
         }}
       />
@@ -133,7 +133,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-TPGTC838"
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TSXDLB9M"
           height="0"
           width="0"
           title="Google Tag Manager"
